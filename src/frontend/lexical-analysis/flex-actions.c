148b8f6a065e484bb1d1ff4f644d3913b54a4233
Martin Zahnd <mzahnd@itba.edu.ar>
2022-04-09 16:17:13 -0300

    Template from agustin-golmar/Flex-Bison-Compiler
    Co-authored-by: Ángeles Ailén Borracci <aborracci@itba.edu.ar>
    Co-authored-by: NeksHinto <nhinojo@itba.edu.ar>
    Co-authored-by: SSanti32 <slarroude@itba.edu.ar>

---
+++ src/frontend/lexical-analysis/flex-actions.c
@@ -0,0 +1,22 @@
+#include "../../backend/logger.h"
+#include "flex-actions.h"
+#include <stdlib.h>
+
+/**
+ * Implementación de "flex-rules.h".
+ */
+
+/* TokenID IntegerPatternAction(const char * lexeme) {
+	LogDebug("IntegerPatternAction: '%s'.", lexeme);
+	yylval = atoi(lexeme);
+	return INTEGER;
+}
+
+void IgnoredPatternAction(const char * lexeme) {
+	LogDebug("IgnoredPatternAction: '%s'.", lexeme);
+}
+
+TokenID UnknownPatternAction(const char * lexeme) {
+	LogDebug("UnknownPatternAction: '%s'.", lexeme);
+	return YYUNDEF;
+} */
