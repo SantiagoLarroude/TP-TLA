cd44dbc8e857cbef20002d3b1757c5fe1b53352a
Martin Zahnd <mzahnd@itba.edu.ar>
2022-04-14 12:27:17 -0300

    Modified nodes. Work in progress.

--- src/frontend/syntactic-analysis/node.h
+++ src/frontend/syntactic-analysis/node.h
@@ -1,6 +1,40 @@
 #ifndef NODE_H
 #define NODE_H
 
+typedef struct node {
+        //
+} node;
+
+typedef struct node_statement {
+        // 
+} node_statement;
+
+typedef struct node_variable_declaration {
+        token_t type;
+        struct node_identifier* id;
+        struct node_expression* assignment_expr;
+} node_variable_declaration;
+
+
+typedef struct node_binary_operator {
+        //
+} node_binary_operator;
+
+typedef struct node_assignment {
+        struct node_identifier* rhs;
+        struct node_expression* lhs;
+} node_assignment;
+
+typedef struct node_identifier {
+        size_t length;
+        char* name;
+} node_identifier;
+
+typedef struct node_block {
+        // 
+        struct node_expression* exp;
+} node_block;
+
 typedef struct node_expression {
         size_t length;
         char* value;
@@ -11,17 +45,6 @@ typedef struct node_expression {
         };
 } node_expression;
 
-typedef struct node_statement {
-        // 
-} node_statement;
 
-typedef struct node_identifier {
-        //
-} node_identifier;
-
-typedef struct node_block {
-        // 
-        struct node_expression* exp;
-} node_block;
 
 #endif  /* NODE_H */
