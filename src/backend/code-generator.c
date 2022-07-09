ec4a28a919cdd6be3d42b45718d853f090b686fb
SSanti32 <slarroude@itba.edu.ar>
2022-07-09 18:26:12 -0300

    Coding code-generator....
    Co-authored-by: borrangie <borrangie@users.noreply.github.com>
    Co-authored-by: Martín E. Zahnd <mzahnd@users.noreply.github.com>
    Co-authored-by: NeksHinto <nhinojo@itba.edu.ar>

--- src/backend/code-generator.c
+++ src/backend/code-generator.c
@@ -6,17 +6,18 @@
 
 //utils with functions to deal with code generation for each specific node type
 
-void generate_type(token_t type);
-void generate_declaration(node_expression * d);
-void generate_assign_declaration (node_expression * ad);
-void generate_assign(node_expression* a);
-void generate_function(node_function* nf);
+void generate_type_code(token_t type);
+void generate_declaration(node_expression * declaration);
+void generate_assign_declaration (node_expression * assign_declaration);
+void generate_assign(node_expression* assignment);
+void generate_function(node_function* function);
 void generate_variable_value(variable * var);
 void generate_return(variable* v);
+static void error_handler(char* msg);
 
 
-char* variable_types[] = {"double", "bool", "char *"};
-void generate_expression_list(node_expression_list* el);
+char* variable_types[] = {"double", "bool", "char *", "int"};
+void generate_expression_list(node_expression_list* expression_list);
 
 void generate_expression(node_expression* expr){
   return;
@@ -28,13 +29,13 @@ void generate_code(program_t * ast)
     generate_function(ast->main_function);
 }
 
-void generate_expression_list(node_expression_list* el)
+void generate_expression_list(node_expression_list* expression_list)
 {
     node_expression* prev;
-    while (el != NULL) {
-        generate_expression(el->expr);
-        prev = el;
-        el = el->next;
+    while (expression_list != NULL) {
+        generate_expression(expression_list->expr);
+        prev = expression_list;
+        expression_list = expression_list->next;
     }
 }
 
@@ -56,59 +57,56 @@ void generate_args(node_list* args)
 void generate_header()
 {
     printf(
-        "#include<stdlib.h>\n"\
-        "#include<stdio.h>\n"\
-        "#include<strings.h>\n"\
+        "#include <stdlib.h>\n"\
+        "#include <stdio.h>\n"\
+        "#include <strings.h>\n"\
         "\n");
 }
 
-void generate_type(token_t type)
+void generate_type_code(token_t type)
 {
     printf("%s", variable_types[type]);
 }
 
-//when called as a line, use ; at the end
-void generate_declaration(node_expression * d)
-{
-    generate_type_code(d->var->type);
-    printf(" %s", d->var->name);
+void generate_assign_declaration (node_expression * assign_declaration) {
+    generate_type_code(assign_declaration->var->type);
+    printf(" ");
+    generate_assign(assign_declaration);
 }
 
-void generate_assign_declaration (node_expression * ad) {
-    generate_type_code(ad->var->type);
-    generate_assign(ad);
-}
-
-void generate_assign(node_expression* a)
+void generate_assign(node_expression* assignment)
 {
-    printf("%s = ", a->var->name);
-    switch(a->type) {
+    printf("%s = ", assignment->var->name);
+    switch(assignment->type) {
         case NUMBER_TYPE: 
         case BOOL_TYPE:
         case STRING_TYPE:
         case FILE_PATH_TYPE:
-            generate_variable_value(a->var);
+            generate_variable_value(assignment->var);
         break;
+        case: VARIABLE_TYPE: // id -> id.
         case EXPRESSION_TYPE:
         case EXPRESSION_GRAMMAR_CONSTANT_TYPE:
         case EXPRESSION_VARIABLE_ASSIGNMENT:
         case EXPRESSION_VARIABLE_DECLARATION:
         case EXPRESSION_FILE_DECLARATION:
-            generate_expression(a);
+            generate_expression(assignment);
         break;
+        default:
+            error_handler("Error - assigning a value or expression to variable");
     }
     printf(";\n");
 }
 
-void generate_function(node_function* nf)
+void generate_function(node_function* function)
 {
-    generate_type_code(nf->return_variable->type);
-    printf(" %s(", nf->name);
-    generate_args(nf->args);
+    generate_type_code(function->return_variable->type);
+    printf(" %s(", function->name);
+    generate_args(function->args);
     printf(")\n");
     printf("{\n");
-    generate_expression_list(nf->expressions);
-    generate_return(nf->return_variable);
+    generate_expression_list(function->expressions);
+    generate_return(function->return_variable);
     printf("}\n");
 }
 
@@ -124,28 +122,99 @@ void generate_variable_value(variable * var)
     case STRING_TYPE:
         printf("%s", var->string);
         break;
+    case FILE_PATH_TYPE:
+        printf("%s", var->string); // TODO
+        break;
+    default:
+        error_handler("Error - generating variable");
     }
+}
+
+//TODO
+void generate_loop(node_expression* loop_node){
+    printf("while(");
+    // condition
+    printf("){\n");
+    // do part()
+    printf("}\n");
+    // printf("for ( %s ;", loop_node->var->id);
+}
+
+void generate_file_handler(node_file_block* block_node){
+
+}
+
+void our_function_columns(){
+
+}
+
+void our_function_lines(){
+
+}
+
+void our_function_byIndex(){
+
+}
+
+void our_function_filter(){
+
+}
+
+void our_function_toString(){
+
+} // medio gila esta, seria printear el nombre de la funcion
+
+void our_function_at(){
+
+}
+
+
+static void error_handler(char* msg){
+    perror(msg);
+    exit(1);
 }
 
 void generate_return(variable* v)
 {
-    printf("return ");
+    printf("return");
     if(v == NULL){
       printf(";\n");
       return;
     }
+    printf(" ");
     switch (v->type)
     {
-      case NUMBER_TYPE:
-      case BOOL_TYPE:
-      case STRING_TYPE:
-        generate_variable_value(v);
-      case ID:
-        if (v->value->expr != NULL)
-            generate_expression(v->value->expr);
+        case NUMBER_TYPE:
+        case BOOL_TYPE:
+        case STRING_TYPE:
+            generate_variable_value(v);
         break;
-      default:
+        case ID:
+            if (v->value->expr != NULL)
+                generate_expression(v->value->expr);
         break;
+        default:
+        error_handler("Error - generating return state");
     }
     printf(";\n");
 }
+
+/*
+    FILE * input_file = fopen("name_file", "r");
+    FILE * output_file = fopen("name_file_2", "w");
+    
+    if(input_file == NULL || output_file == NULL) {
+        error_handler("Error - file handeling failed");
+    }
+
+    int c;
+    while((c = fgetc(input_file)) != EOF) {
+        fprintf(output_file, "%c",c);
+    }
+
+    fprintf(output_file, "%s", input_file);
+
+
+    fclose(input_file);
+    fclose(output_file);
+*/
