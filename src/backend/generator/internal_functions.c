2ee7aa991857b61591a78cee262f524e8ed1c42e
SSanti32 <slarroude@itba.edu.ar>
2022-07-18 09:28:51 -0300

    (bis) changes in r310
    Co-authored-by: Martín E. Zahnd <mzahnd@users.noreply.github.com>

--- src/backend/generator/internal_functions.c
+++ src/backend/generator/internal_functions.c
@@ -73,7 +73,7 @@ static void generate_internal_function_open_file(FILE *const output)
                 "strcat(error_msg, name);"
                 "strcat(error_msg, \"'\");"
 
-                /*caso donde es una carpeta*/
+                /*caso donde es una carpeta
                 "char *is_directory =  strrchr(name, '/');"
                 "if (is_directory != NULL) {"
                 "tex_obj->type = TYPE_T_FILE_LIST;"
@@ -81,7 +81,7 @@ static void generate_internal_function_open_file(FILE *const output)
                 "&tex_obj->value.file.path_list, name);"
                 "tex_obj->value.file.separators = separators;"
                 "return true;"
-                "}"
+                "}"*/
 
                 "FILE *fptr = fopen(name, mode);"
                 "if (fptr == NULL) {"
